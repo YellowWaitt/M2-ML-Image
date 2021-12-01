ba96a47f823cfeecdfb00869d3b58e9ae9139d28
unknown <47664552+YellowWaitt@users.noreply.github.com>
2021-12-01 20:55:33 +0100

    Remove useless stuff

--- path.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-
-root_dir = Path(__file__).parent.as_posix() + "/"
-scripts_dir = root_dir + "automatic-ecg-diagnosis-master/"
-data_dir = root_dir + "data/"
-
-ecg_dir = data_dir + "ecg/"
-ptb_dir = data_dir + "ptb-xl/"
